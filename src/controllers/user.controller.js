c75f7364e9b132efe60e271927cb8a92dec51ae2
Your Name <rupeshyadav9978@gmail.com>
2025-05-30 23:55:08 +0530

    Implements user authentication enhancements
    Adds refresh token functionality for improved session management.
    Includes password change, profile update, and avatar/cover image management features.
    Enhances user security and provides more control over account settings.
    
    Implements user authentication enhancements
    
    Adds refresh token functionality for improved session management and security.
    
    Includes password change and profile update features, enhancing user control over account settings.
    
    Improves user security and provides more control over account settings.

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import {User} from "../models/user.model.js"
 import {upload_on_cloudinary} from "../fileUpload/cloudinary.js";
 import { ApiError } from "../utils/ApiError.js";
+import jwt from "jsonwebtoken"
+import { findAncestor } from "typescript";
 
 const generate_access_refresh_tokens = async (user) => {
     try {
@@ -8,8 +10,8 @@ const generate_access_refresh_tokens = async (user) => {
         const refresh_token = await user.generateRefreshToken()
 
         // Save refresh token into DB
-        user.refresToken = refresh_token
-        user.save({validateBeforeSave: false})
+        user.refreshToken = refresh_token
+        await user.save({validateBeforeSave: false})
 
         return {access_token, refresh_token}
     } catch (error) {
@@ -131,11 +133,11 @@ const login_user = async(req, res) => {
     return res.status(201)
     .cookie("accessToken", access_token, {
         httpOnly: true,
-        secure: false
+        secure: true
     })
     .cookie("refreshToken", refresh_token, {
         httpOnly: true,
-        secure: false
+        secure: true
     })
     .json({loggedIn_user})
 }
@@ -163,8 +165,177 @@ const logout_user = async(req, res) => {
     .json(loggedOut_user)
 }
 
+const renew_access_token = async (req, res) => {
+    const incomingRefreshToken = req.cookies?.refreshToken || req.body.refreshToken || req.header("Authorization")?.replace("Bearer ", "")
+
+    if(!incomingRefreshToken){
+        throw new ApiError(401, "Unauthorized request")
+    }
+
+    try {
+        const decoded_token = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
+    
+        const user = await User.findById(decoded_token._id)
+    
+        if(!user){
+            throw new ApiError(400, "Refresh Token")
+        }
+    
+        if(incomingRefreshToken !== user.refreshToken){
+            throw new ApiError(400, "Refresh token is expired or used")
+        }
+    
+        const {new_access_token, new_refresh_token} = await generate_access_refresh_tokens(user)
+
+        // Push in DB
+        user.refreshToken = new_refresh_token
+        await user.save({validateBeforeSave: false})
+    
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+    
+        return res.status(201)
+        .cookie("accessToken", new_access_token, options)
+        .cookie("refreshToken", new_refresh_token, options)
+        .json({
+            access_token: new_access_token,
+            refreshToken: new_refresh_token
+        })
+    } catch (error) {
+        throw new ApiError(400, error?.message || "Invalid Refresh Token")
+    }
+}
+
+
+const change_user_current_password = async (req, res) => {
+    const {current_password, new_password} = req.body
+
+    const user = await User.findById(req.user?._id)
+
+    if (! await user.isPasswordCorrect(current_password)){
+        throw new ApiError(400, "Wrong current password")
+    }
+
+    if(!new_password){
+        throw new ApiError(400, "Password is required.")
+    }
+    if(new_password.length < 8){
+        throw new ApiError(400, "Password must have at least 8 characters.")
+    }
+
+    user.password = new_password
+    await user.save({validateBeforeSave: false})
+
+    return res.status(201).json({"201": "password changed successfully"})
+} 
+
+const get_current_user = async (req, res) => {    
+    return res.status(200).json({success: true, data: req.user})
+}
+
+const change_user_fullname = async (req, res) => {
+    const {fullName} = req.body
+
+    if(!fullName){
+        throw new ApiError(401, "Invalid Fullname")
+    }
+
+    if (!req.user ){
+        throw new ApiError(401, "Unauthorized User request")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user._id, 
+        {
+            fullName: fullName
+        },
+        {new: true}
+    ).select("-password -refreshToken")
+
+    if(!user){
+        throw new ApiError(404, "User not found")
+    }
+
+    return res.status(200).json({success: true, data: user})
+}
+
+const change_user_avatar = async (req, res) => {
+    let avatar_file_local_path;
+    if(req.files && Array.isArray(req.files.avatar) && req.files.avatar.length > 0){
+        avatar_file_local_path = req.files.avatar[0].path;
+    }
+
+    if(!avatar_file_local_path){
+        throw new ApiError(400, "avatar file is invalid or absent")
+    }
+
+    const avatar = await upload_on_cloudinary(avatar_file_local_path)
+
+    if(!avatar || !avatar.url){
+        throw new ApiError(502, "upload problem on cloudinary")
+    }
+
+    if (!req.user){
+        throw new ApiError(401, "Unauthorized User request")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user._id,
+        {avatar: avatar.url},
+        {new: true}
+    ).select("-password -refreshToken")
+
+    if(!user){
+        throw new ApiError(404, "User not found")
+    }
+
+    return res.status(200).json({success: true, data: user})
+}
+
+const change_user_coverImage = async (req, res) => {
+    let coverImage_file_local_path;
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImage_file_local_path = req.files.coverImage[0].path;
+    }
+
+    if(!coverImage_file_local_path){
+        throw new ApiError(400, "coverImage file is invalid or absent")
+    }
+
+    const coverImage = await upload_on_cloudinary(coverImage_file_local_path)
+
+    if(!coverImage || !coverImage.url){
+        throw new ApiError(500, "upload problem on cloudinary")
+    }
+
+    if (!req.user){
+        throw new ApiError(401, "Unauthorized User request")
+    }
+
+    const user = await User.findByIdAndUpdate(
+        req.user._id,
+        {coverImage: coverImage.url},
+        {new: true}
+    ).select("-password -refreshToken")
+
+    if(!user){
+        throw new ApiError(404, "User not found")
+    }
+
+    return res.status(200).json({success: true, data: user})
+}
+
+
 export {
     register_user,
     login_user,
-    logout_user
+    logout_user,
+    renew_access_token,
+    change_user_current_password,
+    get_current_user,
+    change_user_fullname,
+    change_user_avatar,
+    change_user_coverImage
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import {Router} from "express"
-import {login_user, logout_user, register_user} from "../controllers/user.controller.js"
+import {change_user_current_password, change_user_fullname, login_user, logout_user, register_user, renew_access_token} from "../controllers/user.controller.js"
 import { upload } from "../middlewares/multer.middleware.js"
 import { verify_jwt } from "../middlewares/auth.middleware.js"
 
@@ -20,7 +20,13 @@ user_router.route("/register").post(
 )
 
 user_router.route("/login").post(login_user)
+
+// secured Routes
 user_router.route("/logout").post(verify_jwt, logout_user)
+user_router.route("/refresh-token").post(renew_access_token)
+user_router.route("/change-user-password").post(verify_jwt, change_user_current_password)
+user_router.route("/change-user-fullName").post(verify_jwt, change_user_fullname)
+user_router.route("/change-user-avatar").post(verify_jwt, upload.single("avatar") , change_user_avatar)
 
 
 export {user_router}
